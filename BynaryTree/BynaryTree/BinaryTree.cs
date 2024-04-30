172bda49b4f2ca896e786b83b279b840c260e9ea
Jair Morais <Jair.Morais@anm.gov.br>
2024-04-30 10:12:11 -0300

    Binary tree with search and grph tree

---
+++ BynaryTree/BynaryTree/BinaryTree.cs
@@ -0,0 +1,96 @@
+﻿namespace BynaryTree
+{
+    public class BinaryTree
+    {
+        public TreeNode? Root { get; set; }
+
+        public BinaryTree()
+        {
+            Root = null;
+        }
+
+        public void Insert(int value)
+        {
+            Root = InsertRec(Root, value);
+        }
+
+        private TreeNode InsertRec(TreeNode? root, int value)
+        {
+            if (root == null)
+            {
+                root = new TreeNode(value);
+                return root;
+            }
+
+            if (value < root.Value)
+            {
+                root.Left = InsertRec(root.Left, value);
+            }
+            else
+            {
+                root.Right = InsertRec(root.Right, value);
+            }
+
+            return root;
+        }
+
+        public void InOrderTraversal(TreeNode root)
+        {
+            if (root != null)
+            {
+                InOrderTraversal(root.Left!);
+                Console.Write(root.Value + " ");
+                InOrderTraversal(root.Right!);
+            }
+        }
+
+        public void PrintTree()
+        {
+            PrintTreeRec(Root, 0);
+        }
+
+        private void PrintTreeRec(TreeNode root, int depth)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            const int SpacesPerLevel = 4;
+
+            PrintTreeRec(root.Right, depth + 1);
+
+            Console.Write(new string(' ', depth * SpacesPerLevel));
+            Console.WriteLine(root.Value);
+
+            PrintTreeRec(root.Left, depth + 1);
+        }
+
+        public bool Search(int value)
+        {
+            return SearchRec(Root, value);
+        }
+
+        private bool SearchRec(TreeNode root, int value)
+        {
+            if (root == null)
+            {
+                return false;
+            }
+
+            if (root.Value == value)
+            {
+                return true;
+            }
+            else if (value < root.Value)
+            {
+                return SearchRec(root.Left!, value);
+            }
+            else
+            {
+                return SearchRec(root.Right!, value);
+            }
+
+        }
+    }
+}
---
+++ BynaryTree/BynaryTree/Program.cs
@@ -0,0 +1,32 @@
+﻿using BynaryTree;
+
+BinaryTree tree = new BinaryTree();
+
+tree.Insert(50);
+tree.Insert(10);
+tree.Insert(20);
+tree.Insert(80);
+tree.Insert(30);
+tree.Insert(5);
+tree.Insert(90);
+tree.Insert(40);
+
+Console.WriteLine("Árvore em ordem (in-order traversal):");
+tree.InOrderTraversal(tree.Root);
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine("Árvore em ordem Tree:");
+tree.PrintTree();
+Console.WriteLine();
+
+int valorBusca = 80;
+bool encontrado = tree.Search(valorBusca);
+
+if (encontrado)
+{
+    Console.WriteLine($"O valor {valorBusca} está presente na árvore.");
+}
+else
+{
+    Console.WriteLine($"O valor {valorBusca} não está presente na árvore.");
+}
